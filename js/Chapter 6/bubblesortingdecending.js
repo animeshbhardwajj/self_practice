d9b82b96a709450d1ff2a0caa8085dd1b3bdbbcd
Animesh Bhardwaj <animesh0422@gmail.com>
2023-01-23 22:25:54 +0530

    idk

--- js/Chapter 6/bubblesortingdecending.js
+++ js/Chapter 6/bubblesortingdecending.js
@@ -5,7 +5,7 @@ var a = [36,19,29,12,5];
 
  for(var i = 0; i < a.length; i++){
   for(var j = 0; j < ( a.length - i -1 ); j++){
-    if(a[j] < a[j+1]){
+    if(a[j] > a[j+1]){
      var temp = a[j];
      a[j] = a[j + 1];
      a[j+1] = temp;
